e00b02a55d56a34122ceaf91f1328db036e4827b
Oliver Flyckt Wilhjelm <olfw@itu.dk>
2022-09-21 11:14:52 +0200

    delegation tests added

--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -6,5 +6,24 @@ public class DelegatesTests
     public void Test1()
     {
 
+        Func<double, double, double> product = (number1, number2) => number1*number2;
+        
+        var check = product(5, 3);
+
+        check.Should().Be(15);
+       
+        Func<string, string> reverse = str => new string(str.Reverse().ToArray());
+
+        var check1 = reverse("a string");
+
+        check1.Should().Be("gnirts a");
+
+
+        Func<string, int, bool> isEqual = (str, number) => int.Parse(str) == number;
+
+        var check2 = isEqual("0042", 42);
+
+        check2.Should().Be(true);
     }
+
 }
