df7eba45eceab6fca5a1f1c3f5c516d2e75100e5
tcla <tcla@itu.dk>
2022-09-22 09:34:36 +0200

    LINQ tasks about wizard completed and tested

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -3,8 +3,105 @@ namespace Assignment2.Tests;
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void Test_Wizards_By_Rowling()
     {
-
+        var exp = new string[] {"Albus Dumbledore", "Severus Snape", "Harry James Potter",
+        "Hermione Jean Granger", "Ronald Bilius Weasley", "Sirius Black", "Minerva McGonagall"};
+        var res = new Queries().Wizards_By_Rowling();
+        var val = res.Intersect(exp);
+        val.Count().Should().Be(exp.Count());
     }
+    [Fact]
+    public void Test_Wizards_By_Rowling_Linq()
+    {
+        var exp = new string[] {"Albus Dumbledore", "Severus Snape", "Harry James Potter",
+        "Hermione Jean Granger", "Ronald Bilius Weasley", "Sirius Black", "Minerva McGonagall"};
+        var res = new Queries().Wizards_By_Rowling_Linq();
+        var val = res.Intersect(exp);
+        val.Count().Should().Be(exp.Count());
+    }
+
+    [Fact]
+    public void Test_The_First_Sith()
+    {
+        var res = new Queries().The_First_Sith();
+        res.Should().Be(1977);
+    }
+    [Fact]
+    public void Test_The_First_Sith_Linq()
+    {
+        var res = new Queries().The_First_Sith_Linq();
+        res.Should().Be(1977);
+    }
+
+    [Fact]
+    public void Test_Unique_HP_Wizards()
+    {
+        var exp = new (string Name, int? Year)[] {("Albus Dumbledore", 1998), ("Severus Snape", 1998), ("Harry James Potter", 1998),
+        ("Hermione Jean Granger", 1998), ("Ronald Bilius Weasley", 1998), ("Sirius Black", 1998), ("Minerva McGonagall", 1998)};
+        var res = new Queries().Unique_HP_Wizards();
+        var val = res.Intersect(exp);
+        val.Count().Should().Be(exp.Count());
+    }
+    [Fact]
+    public void Test_Unique_HP_Wizards_Linq()
+    {
+        var exp = new (string Name, int? Year)[] {("Albus Dumbledore", 1998), ("Severus Snape", 1998), ("Harry James Potter", 1998),
+        ("Hermione Jean Granger", 1998), ("Ronald Bilius Weasley", 1998), ("Sirius Black", 1998), ("Minerva McGonagall", 1998)};
+        var res = new Queries().Unique_HP_Wizards_Linq();
+        var val = res.Intersect(exp);
+        val.Count().Should().Be(exp.Count());
+    }
+
+    [Fact]
+    public void Test_Wizards_And_Creators()
+    {
+        var exp = new (string Creator, string Name)[] {
+            ("J.R.R. Tolkien", "Sauron"),
+            ("J.R.R. Tolkien", "Radagast"),
+            ("J.R.R. Tolkien", "Gandalf"),
+            ("J.K. Rowling", "Sirius Black"),
+            ("J.K. Rowling", "Severus Snape"),
+            ("J.K. Rowling", "Ronald Bilius Weasley"),
+            ("J.K. Rowling", "Minerva McGonagall"),
+            ("J.K. Rowling", "Hermione Jean Granger"),
+            ("J.K. Rowling", "Harry James Potter"),
+            ("J.K. Rowling", "Albus Dumbledore"),
+            ("George Lucas", "Darth Vader"),
+            ("George Lucas", "Darth Maul")
+        };
+        var res = new Queries().Wizards_And_Creators();
+        int i = 0;
+        foreach (var w in res)
+        {
+            w.Should().Be(exp[i]);
+            i++;
+        }
+    }
+    [Fact]
+    public void Wizards_And_Creators_Linq()
+    {
+        var exp = new (string Creator, string Name)[] {
+            ("J.R.R. Tolkien", "Sauron"),
+            ("J.R.R. Tolkien", "Radagast"),
+            ("J.R.R. Tolkien", "Gandalf"),
+            ("J.K. Rowling", "Sirius Black"),
+            ("J.K. Rowling", "Severus Snape"),
+            ("J.K. Rowling", "Ronald Bilius Weasley"),
+            ("J.K. Rowling", "Minerva McGonagall"),
+            ("J.K. Rowling", "Hermione Jean Granger"),
+            ("J.K. Rowling", "Harry James Potter"),
+            ("J.K. Rowling", "Albus Dumbledore"),
+            ("George Lucas", "Darth Vader"),
+            ("George Lucas", "Darth Maul")
+        };
+        var res = new Queries().Wizards_And_Creators_Linq();
+        int i = 0;
+        foreach (var w in res)
+        {
+            w.Should().Be(exp[i]);
+            i++;
+        }
+    }
+
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,4 +2,91 @@ namespace Assignment2;
 
 public class Queries
 {
+    public IEnumerable<string> Wizards_By_Rowling()
+    {
+        var wizards = WizardCollection.Create();
+        var rowlingWizards = wizards
+            .Where(w => w.Creator.Contains("Rowling"))
+            .Select(w => w.Name);
+        return rowlingWizards;
+    }
+    public IEnumerable<string> Wizards_By_Rowling_Linq()
+    {
+        var wizards = WizardCollection.Create();
+        var rowlingWizards = from w in wizards
+                             where w.Creator.Contains("Rowling")
+                             select w.Name;
+        return rowlingWizards;
+    }
+
+    public int The_First_Sith()
+    {
+        var wizards = WizardCollection.Create();
+        var siths = wizards
+            .Where(w => w.Name.StartsWith("Darth") && w.Year != null)
+            .Select(w => (w.Year));
+        int theFirst = (int)siths.Min()!;
+        return theFirst;
+    }
+    public int The_First_Sith_Linq()
+    {
+        var wizards = WizardCollection.Create();
+        var siths = from w in wizards
+                    where w.Name.StartsWith("Darth") && w.Year != null
+                    select w.Year;
+        int theFirst = (int)siths.Min()!;
+        return theFirst;
+    }
+
+    public IEnumerable<(string Name, int? Year)> Unique_HP_Wizards()
+    {
+        var HPBooks = new string[] {
+            "Philosopher's Stone",
+            "Chamber of Secrets",
+            "Prisoner of Azkaban",
+            "Goblet of Fire",
+            "Order of the Phoenix",
+            "Half-Blood Prince",
+            "Deathly Hallows",
+        };
+        var wizards = WizardCollection.Create();
+        var HPWizards = wizards
+            .Where(w => HPBooks.Contains(w.Medium))
+            .Distinct().Select(w => (w.Name, w.Year));
+        return HPWizards;
+    }
+    public IEnumerable<(string Name, int? Year)> Unique_HP_Wizards_Linq()
+    {
+        var HPBooks = new string[] {
+            "Philosopher's Stone",
+            "Chamber of Secrets",
+            "Prisoner of Azkaban",
+            "Goblet of Fire",
+            "Order of the Phoenix",
+            "Half-Blood Prince",
+            "Deathly Hallows",
+        };
+        var wizards = WizardCollection.Create();
+        var HPWizards = from w in wizards
+                        where HPBooks.Contains(w.Medium)
+                        select (w.Name, w.Year);
+        return HPWizards;
+    }
+
+    public IEnumerable<(string Creator, string Name)> Wizards_And_Creators() {
+        var wizards = WizardCollection.Create();
+        var grouped = wizards
+            .GroupBy(w => (w.Creator, w.Name))
+            .OrderByDescending(g => g.Key)
+            .Select(w => w.Key);
+        return grouped;
+    }
+    public IEnumerable<(string Creator, string Name)> Wizards_And_Creators_Linq() {
+        var wizards = WizardCollection.Create();
+        var grouped = from w in wizards
+                        group w by (w.Creator, w.Name) into g 
+                        orderby g.Key descending 
+                        select g.Key;
+        return grouped;
+    }
 }
